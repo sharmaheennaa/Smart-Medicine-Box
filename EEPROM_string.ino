22c9d24c1dd805ac83a87e15c3d201059d01febb
sharmaheennaa <sharmaheennaa@gmail.com>
2020-10-02 20:04:26 +0530

    Add files via upload

---
+++ EEPROM_string.ino
@@ -0,0 +1,56 @@
+#include <EEPROM.h>
+ 
+void writeString(char add,String data);
+String read_String(char add);
+ 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  EEPROM.begin(512);
+  String data = "0600";
+ 
+  Serial.print("Writing Data:");
+  Serial.println(data);
+ 
+  writeString(10, data);  //Address 10 and String type data
+  delay(10);
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+  String recivedData;
+  recivedData = read_String(10);
+  Serial.print("Read Data:");
+  Serial.println(recivedData);
+  delay(1000);
+}
+ 
+void writeString(char add,String data)
+{
+  int _size = data.length();
+  int i;
+  for(i=0;i<_size;i++)
+  {
+    EEPROM.write(add+i,data[i]);
+  }
+  EEPROM.write(add+_size,'\0');   //Add termination null character for String Data
+  EEPROM.commit();
+}
+ 
+ 
+String read_String(char add)
+{
+  int i;
+  char data[100]; //Max 100 Bytes
+  int len=0;
+  unsigned char k;
+  k=EEPROM.read(add);
+  while(k != '\0' && len<500)   //Read until null character
+  {    
+    k=EEPROM.read(add+len);
+    data[len]=k;
+    len++;
+  }
+  data[len]='\0';
+  return String(data);
+}
---
+++ RTC.ino
@@ -0,0 +1,41 @@
+#include <WProgram.h>
+#include <Wire.h>
+#include <DS1307.h> 
+#include "RTClib.h"
+
+RTC_DS1307 rtc;
+
+void setup()
+{
+  Serial.begin(9600);
+  RTC.stop();
+  RTC.set(DS1307_SEC,1);        //set the seconds
+  RTC.set(DS1307_MIN,20);     //set the minutes
+  RTC.set(DS1307_HR,17);       //set the hours
+  RTC.set(DS1307_DOW,4);       //set the day of the week
+  RTC.set(DS1307_DATE,1);       //set the date
+  RTC.set(DS1307_MTH,6);        //set the month
+  RTC.set(DS1307_YR,12);         //set the year
+  RTC.start();
+  //pinMode(10,OUTPUT);
+  
+  
+}
+void loop()
+{
+  
+  Serial.print(RTC.get(DS1307_HR,true)); //read the hour and also update all the values by pushing in true
+  Serial.print(":");
+  Serial.print(RTC.get(DS1307_MIN,false));//read minutes without update (false)
+  Serial.print(":");
+  Serial.print(RTC.get(DS1307_SEC,false));//read seconds
+  Serial.print("      ");                 // some space for a more happy life
+  Serial.print(RTC.get(DS1307_DATE,false));//read date
+  Serial.print("/");
+  Serial.print(RTC.get(DS1307_MTH,false));//read month
+  Serial.print("/");
+  Serial.print(RTC.get(DS1307_YR,false)); //read year
+  Serial.println();
+  delay(1000);
+  
+}
